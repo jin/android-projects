8ec9c1bb653277a0d6d22d52b5d13b5de173e332
Jingwen Chen <jingwen@google.com>
2018-04-29 16:29:03 -0400

    woops

---
+++ rectangle/androidAppModule6/src/main/java/com/androidAppModule6/Activity1.java
@@ -0,0 +1,12 @@
+package com.androidAppModule6;
+
+import java.lang.Override;
+
+public class Activity1 extends android.app.Activity {
+  @Override
+  public void onCreate(android.os.Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    new androidAppModule6packageJava0.Foo0().foo3();
+    setContentView(R.layout.androidappmodule6activity_main1);
+  }
+}
