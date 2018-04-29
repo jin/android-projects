447840828b4196d3f3d127c5cf10f913555b7445
Jingwen Chen <jingwen@google.com>
2018-04-29 19:37:04 -0400

    verbose update graph

--- update_graphs.sh
+++ update_graphs.sh
@@ -2,6 +2,15 @@
 # usage after generating a new project with `bazel run :android_studio_poet`
 # ./update_graphs $SUBDIRECTORY
 
+set -euo pipefail
+
 pushd $1; shift
-bazel query 'deps(//androidAppModule0)' --output=graph --noimplicit_deps > bazel_graph.dot && dot -Tpng bazel_graph.dot -o bazel_graph.png &&  dot -Tpng dependencies.dot -o gradle_graph.png
+echo "Querying]"
+bazel query 'deps(//androidAppModule0)' --output=graph --noimplicit_deps > bazel_graph.dot
+
+echo "creating Bazel graph]"
+dot -v -Tpng bazel_graph.dot -o bazel_graph.png
+
+echo "creating Gradle graph]"
+dot -v -Tpng dependencies.dot -o gradle_graph.png
 popd
