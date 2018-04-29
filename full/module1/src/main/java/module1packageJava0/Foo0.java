fbe010a30662dd9a04c9278bab4402d518600d1e
Jingwen Chen <jingwen@google.com>
2018-04-29 18:19:59 -0400

    Update full to break dependency cycle

--- full/module1/src/main/java/module1packageJava0/Foo0.java
+++ full/module1/src/main/java/module1packageJava0/Foo0.java
@@ -3,7 +3,6 @@ package module1packageJava0;
 public class Foo0 {
   public void foo0() {
     final Runnable anything = () -> System.out.println("anything");
-    new module0packageJava0.Foo4().foo3();
     new module2packageJava0.Foo4().foo3();
     new module3packageJava0.Foo4().foo3();
     new module4packageJava0.Foo4().foo3();
