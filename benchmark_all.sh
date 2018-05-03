db4210e6c3e61c71f8099aa29c8c8071e859055a
Jingwen Chen <jingwen@google.com>
2018-05-02 20:10:08 -0400

    Downgrade Android plugin to 3.0.0 to fix https://stackoverflow.com/questions/46971946/unable-to-load-class-com-android-builder-version-after-upgrade-to-android-stud

--- benchmark_all.sh
+++ benchmark_all.sh
@@ -5,6 +5,13 @@ set -euo pipefail
 projects=$(ls -d */ | grep -v 'third_party')
 
 for project in $projects; do
+  # Make sure gradle is downloaded first
+  pushd $project
+  ./gradlew
+  popd
+done
+
+for project in $projects; do
   if [ -e "$project/benchmark.sh" ]; then
     pushd $project
     ./benchmark.sh
