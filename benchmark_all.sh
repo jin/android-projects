5f2381a8738a723af419709c9e53b2c2ddf58153
Jingwen Chen <jingwen@google.com>
2018-05-02 21:39:38 -0400

    Grammar

--- benchmark_all.sh
+++ benchmark_all.sh
@@ -7,7 +7,7 @@ projects=$(ls -d */ | grep -v 'third_party')
 for project in $projects; do
   # Make sure gradle and Bazel are runnable
   pushd $project
-  echo "Verifying that Gradle and Bazel works in $project.."
+  echo "Verifying that Gradle and Bazel are working in $project.."
   ./gradlew >/dev/null
   bazel query //androidAppModule0 >/dev/null
   popd
