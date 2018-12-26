f3c7e3cd4678c0e9ec6060e36fbcd19fdaab7664
Jingwen Chen <jingwen@google.com>
2018-12-26 16:52:48 -0500

    Update android-studio-poet uber jar

--- regenerate_projects.sh
+++ regenerate_projects.sh
@@ -6,7 +6,7 @@ projects=$(ls -d */ | grep -v 'third_party')
 
 for project in $projects; do
   cp $project/performance.scenarios /tmp/tmp_scenarios
-  java -jar third_party/android-studio-poet-all-1.0-SNAPSHOT-BAZEL-EXTENSIONS.jar $project/as_poet_config.json
+  java -jar third_party/android-studio-poet-all.jar $project/as_poet_config.json
   ./prepare_project.sh $project
   cp /tmp/tmp_scenarios $project/performance.scenarios
 done
